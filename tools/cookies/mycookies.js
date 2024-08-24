f8b17fc92c41dc8267958171d7292af347d8022a
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-08-24 17:57:46 +0300

    Update mycookies.js

--- tools/cookies/mycookies.js
+++ tools/cookies/mycookies.js
@@ -12,23 +12,36 @@ document.addEventListener('DOMContentLoaded', function() {
 function deleteAllCookies() {
     // Λήψη όλων των cookies
     var cookies = document.cookie.split(';');
+    
+    // Δημιουργία ενός πίνακα για καταγραφή των cookies που θα διαγραφούν
+    var cookiesToDelete = [];
 
     for (var i = 0; i < cookies.length; i++) {
         var cookie = cookies[i];
         var eqPos = cookie.indexOf('=');
         var name = eqPos > -1 ? cookie.substr(0, eqPos).trim() : cookie.trim();
+        var value = eqPos > -1 ? cookie.substr(eqPos + 1).trim() : '';
+
+        // Καταγραφή του cookie που πρόκειται να διαγραφεί
+        cookiesToDelete.push({ name: name, value: value });
 
         // Διαγραφή κάθε cookie με ημερομηνία λήξης στο παρελθόν
         document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/';
         document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/; domain=' + location.hostname;
+        document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/; domain=' + location.hostname + '; Secure';
+        document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/; domain=' + location.hostname + '; Secure; SameSite=None';
     }
 
-    // Επιβεβαίωση ότι τα cookies έχουν διαγραφεί
-    console.log('Όλα τα cookies έχουν διαγραφεί.');
+    // Επιβεβαίωση ότι τα cookies έχουν διαγραφεί με καταγραφή
+    console.log('Όλα τα cookies έχουν διαγραφεί:');
+    cookiesToDelete.forEach(function(cookie) {
+        console.log('Cookie Name:', cookie.name, '| Value:', cookie.value);
+    });
 
     // Ενημέρωση των στοιχείων td μετά τη διαγραφή των cookies
     updateCookieValues();
 }
+
 
 function updateCookieValues() {
     // Λήψη των cookies
