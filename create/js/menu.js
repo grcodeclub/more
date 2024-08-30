959946f577f14383756375892a9b23ba9c34233d
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-08-30 13:37:55 +0300

    Update menu.js

--- create/js/menu.js
+++ create/js/menu.js
@@ -1,9 +1,9 @@
 window.onload = function() {
     var menuLinks = [
         '<li><a href="https://grcodeclub.gr">Αρχική Σελίδα</a></li>',
-        '<li><a href="https://grcodeclub.gr/create/">Manual</a></li>',
-        '<li><a href="https://grcodeclub.gr/create/github">GitHub</a></li>',
-        '<li><a href="https://grcodeclub.gr/create/google">Υπηρεσίες Google</a></li>',
+        '<li><a href="https://grcodeclub.gr/more/create/">Manual</a></li>',
+        '<li><a href="https://grcodeclub.gr/more/create/github">GitHub</a></li>',
+        '<li><a href="https://grcodeclub.gr/more/create/google">Υπηρεσίες Google</a></li>',
         '<li style="height: 200px;"></li>'
     ];
     
