df3d476d87310704e7c20e0f18b9f641dcd467d8
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-09-30 07:00:57 -0400

    Update input.js

--- create/post/input.js
+++ create/post/input.js
@@ -2,21 +2,36 @@ function addNewItem() {
     const container = document.getElementById("inputContainer");
     const newItem = document.createElement("div");
     newItem.classList.add("item");
-    
-    newItem.innerHTML = `
-                            <h3 class="highlight">  <input type="text" class="unity" placeholder="Εισάγετε τίτλο"/></h3>
 
-                    <label for="content">Κείμενο:</label>
-                    <textarea class="content" placeholder="Εισάγετε κείμενο"></textarea><br><br>
+    const selectedValue = document.getElementById("itemSelect").value;
+    const selectElement = document.getElementById("itemSelect"); // Επιλογή του select
 
-                    <label for="image">Εικόνα (URL):</label>
-                    <input type="text" class="image" placeholder="Εισάγετε URL εικόνας" />
+    if (selectedValue === "title") {
+        newItem.innerHTML = `
+          <label for="content">Εισάγετε τίτλο Ενοτητας:</label>
+            <h3 class="highlight">  <input type="text" class="unity" placeholder="Εισάγετε τίτλο Ενοτητας"/></h3>
+            <button onclick="removeItem(this)" class="mybutton">Αφαίρεση</button><br><br>`;
+    } else if (selectedValue === "text") {
+        newItem.innerHTML = `
+            <label for="content">Κείμενο:</label>
+            <p>  <textarea class="content" placeholder="Εισάγετε Κείμενο"/></textarea></p>
+            <button onclick="removeItem(this)" class="mybutton">Αφαίρεση</button><br><br>`;
+    } else if (selectedValue === "image") {
+        newItem.innerHTML = `
+            <label for="image">Εικόνα (URL):</label>
+            <input type="text" class="image" placeholder="Εισάγετε URL εικόνας" />
+            <button onclick="removeItem(this)" class="mybutton">Αφαίρεση</button><br><br>`;
+    }
+    else if (selectedValue === "mycode") {
+        newItem.innerHTML = `
+            <label for="image">Κωδικας</label>
+            <textarea class="mycode" placeholder="Εισάγετε Κωδικα"/></textarea>
+            <button onclick="removeItem(this)" class="mybutton">Αφαίρεση</button><br><br>`;
+    }
 
-                    <br><br>
-                    <button onclick="removeItem(this)" class="mybutton">Αφαίρεση</button><br><br>
-    `;
-
-    container.appendChild(newItem);
+    // Εύρεση του γονέα του select για να γίνει insert ακριβώς πάνω από το select
+    const parentElement = selectElement.parentElement;
+    parentElement.insertBefore(newItem, selectElement);
 }
 
 function removeItem(button) {
