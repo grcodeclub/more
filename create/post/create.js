7c13a90ce5c772a40a60898835b6385a8bad3776
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-09-30 05:34:30 -0400

    Update create.js

--- create/post/create.js
+++ create/post/create.js
@@ -1,29 +1,3 @@
-function addNewItem() {
-    const container = document.getElementById("inputContainer");
-    const newItem = document.createElement("div");
-    newItem.classList.add("item");
-    
-    newItem.innerHTML = `
-                            <h3 class="highlight">  <input type="text" class="unity" placeholder="Εισάγετε τίτλο"/></h3>
-
-                    <label for="content">Κείμενο:</label>
-                    <textarea class="content" placeholder="Εισάγετε κείμενο"></textarea><br><br>
-
-                    <label for="image">Εικόνα (URL):</label>
-                    <input type="text" class="image" placeholder="Εισάγετε URL εικόνας" />
-
-                    <br><br>
-                    <button onclick="removeItem(this)" class="mybutton">Αφαίρεση</button><br><br>
-    `;
-
-    container.appendChild(newItem);
-}
-
-function removeItem(button) {
-    const item = button.parentElement;
-    item.remove();
-}
-
 function downloadHTML() {
     const htmlContent = generateFinalHTML(); // Καλεί τη συνάρτηση που παράγει το HTML
     const blob = new Blob([htmlContent], { type: 'text/html' });
